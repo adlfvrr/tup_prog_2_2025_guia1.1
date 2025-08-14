8f6fdccf3014ee3339c9a9fc84a762a7eac8b9ea
adlfvrr <adolfover4@gmail.com>
2025-08-14 10:40:38 -0300

    -

---
+++ GUIA_1.1/ej2/Models/Viaje.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej2.Models
+{
+    internal class Viaje
+    {
+        Colectivo colectivo;
+        public Viaje(int hInicio, int mInicio, int asientos)
+        {
+            colectivo = new Colectivo(asientos);
+        }
+    }
+}
